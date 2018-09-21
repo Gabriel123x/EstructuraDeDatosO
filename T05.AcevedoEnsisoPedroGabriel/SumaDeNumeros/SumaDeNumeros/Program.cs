bfbf2a30fe92b46496ffee297d65397e4ee31c33
Gabriel123x <43393807+Gabriel123x@users.noreply.github.com>
2018-09-21 12:04:38 -0700

    Add files via upload

---
+++ T05.AcevedoEnsisoPedroGabriel/SumaDeNumeros/SumaDeNumeros/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaDeNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan stop;
+            TimeSpan start = new TimeSpan(DateTime.Now.Ticks);
+            int Numero = 0;
+            for (int i = 1; i <= 1000000; i++)
+            {
+                Numero = Numero + i;
+            }
+            stop = new TimeSpan(DateTime.Now.Ticks);
+            Console.WriteLine(stop.Subtract(start).TotalMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
