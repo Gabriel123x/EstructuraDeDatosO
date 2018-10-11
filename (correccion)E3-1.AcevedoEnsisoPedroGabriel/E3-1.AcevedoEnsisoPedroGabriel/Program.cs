8013c13d8d0ad1bf81cb6d271483dd7de6b2b2f8
Gabriel123x <43393807+Gabriel123x@users.noreply.github.com>
2018-10-10 17:39:50 -0700

    Add files via upload

---
+++ (correccion)E3-1.AcevedoEnsisoPedroGabriel/E3-1.AcevedoEnsisoPedroGabriel/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_1.AcevedoEnsisoPedroGabriel
+{
+    class Program
+    {
+        //Instrucciones de uso del programa
+        //1)la manera en que funciona el programa es que al principio se te pedira cuantas clases quieres
+        //2)Despues el programa te pedira la materia que se imparte en esa clase y el numero de alumnos que la cursan
+        //3)Depues de introducir estos dos datos se te pedira que agregues los nombres de los alumnos de cada clase seguido de sus calificaciones respectivas
+        //4)Por ultimo uno debe simplemente presionar enter para imprimir las listas de calificacion de cada clase
+        static void Main(string[] args)
+        {
+            ArrayList Tecnologico = new ArrayList();//Lista de Objetos tipo Salon esto ayudo bastante a hacer el programa mas sencillo
+            Salon Aula;//objeto tipo salon que se usa cuando se quiere agregar una nueva clase
+            bool Activo = true;//variable que determina cuando se detiene el programa
+            int TotalClases;
+            string nombre;
+            do//aqui comienza el ciclo del programa siempre y cuando Activo sea verdadera el programa seguira ejecutandose
+            {
+                try
+                {
+                    Console.WriteLine("Cuantas clases tiene la escuela?");
+                    TotalClases = int.Parse(Console.ReadLine());
+                    if (TotalClases < 0)
+                        throw new Exception("El numero introducido aqui debe ser mayor a cero");//excepcion que se usa para asegurarse de que lo introducido sea mayor a cero
+                    for (int i = 0; i < TotalClases; i++)//En este ciclo es donde agregamos cada clase a la lista de salones
+                    {                                    //dependiendo de cuantas clases introdujo el usuario son las veces que el ciclo generara salones
+                        Console.WriteLine("Cual es la clase que se imparte en el salon {0}? clases restantes {1}",(i + 1),TotalClases - (i + 1));
+                        nombre = Console.ReadLine();//primero el usuario introduce el nombre de la clase
+                        Aula = new Salon(nombre);//aqui creamos el salon con los datos introducidos
+                        Tecnologico.Add(Aula);//agregamos el salon a la lista
+                    }
+                    Console.Clear();
+                    foreach(Salon i in Tecnologico)//en este ciclo usamos los metodos PedirAlumnos, PedirCalificaciones y LeerNumeroDeAlumnos de cada objeto Salon en la lista
+                    {
+                        i.LeerNumeroDeAlumnos();
+                        i.PedirAlumnos();
+                        i.PedirCalificaciones();
+                    }
+                    Console.WriteLine("Los datos de cada clase han sido agregados");
+                    Console.WriteLine("A continuacion se imrimen las calificaciones de cada clase presione enter");
+                    Console.ReadKey();
+                    Console.Clear();
+                    foreach(Salon i in Tecnologico)//en este ciclo usamos el metodo Imprimir calificaciones para imprimir las calificaciones de cada objeto salon que se encuentra en la lista
+                    {
+                        i.ImprimirCalificaciones();
+                    }
+                    Console.WriteLine("Fin del programa presione enter para salir");
+                    Activo = false;//cambiando el valor de esta variable terminamos el programa
+
+                }
+                catch (Exception E)//excepcion general para atrapar excepciones no previstas
+                {
+                    Console.WriteLine(E.Message);
+                }
+                finally
+                {
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            } while (Activo == true);
+        }
+    }
+}
---
+++ (correccion)E3-1.AcevedoEnsisoPedroGabriel/E3-1.AcevedoEnsisoPedroGabriel/Salon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_1.AcevedoEnsisoPedroGabriel
+{
+    class Salon
+    {
+        public string NombreClase;//Variable que guarda el nombre de la materia impartida en el salon 
+        public int NumeroDeDatos;
+        public List<string> Alumnos;//Aqui guardamos los alumnos de la clase
+        public List<int> Calificaciones;//Aqui guardamos las calificaciones de cada alumnos
+        public Salon(string nombre)//Constructor
+        {
+            Alumnos = new List<string>();
+            Calificaciones = new List<int>();
+            NombreClase = nombre;
+        }
+        public void LeerNumeroDeAlumnos()//Metodo en el que simplemente le pedimos al usuario el numero de alumnos que cursan en este salon
+        {
+            Console.WriteLine("Cuantos Alumnos hay en el salon de {0}", NombreClase);
+            NumeroDeDatos = int.Parse(Console.ReadLine());
+            if (NumeroDeDatos < 0)
+                throw new Exception("El numero introducido debe ser mayor a cero");
+            Console.WriteLine("Numero Agregado");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public void PedirAlumnos()//metodo que le pide al usuario el nombre de cada alumno que cursa la clase 
+        {
+            for(int i = 1; i <= NumeroDeDatos;i++)
+            {
+                Console.WriteLine("Introduccion de Alumnos de la clase de {0}", NombreClase);
+                Console.WriteLine("Introduce el alumno numero {0} alumnos restantes {1}",i,NumeroDeDatos - i);
+                Alumnos.Add(Console.ReadLine());
+                Console.Clear();
+            }
+            Console.WriteLine("Alumnos del salon de {0} agregados",NombreClase);
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public void PedirCalificaciones()//Metodo que recorre cada alumno de esta clase y le da su calificacion introducida por el usuario
+        {
+            for (int i = 1; i <= Alumnos.Count; i++)
+            {
+                Console.WriteLine("Introduccion de calificaciones de la clase de {0}", NombreClase);
+                Console.WriteLine("Introdusca la calificacion del alumno {0} Alumnos restantes {1}", Alumnos[i - 1], Alumnos.Count - i);
+                Calificaciones.Add(int.Parse(Console.ReadLine()));
+                if (Calificaciones[i - 1] < 0 || Calificaciones[i - 1] > 100)
+                    throw new Exception("La calificacion debe ser mayor o igual a cero y no sobrepasar 100");
+                Console.Clear();
+            }
+            Console.WriteLine("Calificacioes del salon de {0} agregadas", NombreClase);
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public void ImprimirCalificaciones()//metodo que imprime la calificacion de cada alumno de la clase
+        {
+            Console.WriteLine("Calificaciones de la materia de {0}", NombreClase);
+            for (int i = 0; i < NumeroDeDatos; i++)
+            {
+                Console.SetCursorPosition(1,i + 1);
+                Console.Write(Alumnos[i]);
+                Console.SetCursorPosition(50, i + 1);
+                Console.Write(Calificaciones[i]);
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
