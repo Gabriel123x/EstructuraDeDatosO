99e2b5194e450f2dffd64a3813d57560fe096c0d
Gabriel123x <43393807+Gabriel123x@users.noreply.github.com>
2018-11-07 17:36:24 -0800

    Add files via upload

---
+++ E4-1.AcevedoEnsisoPedroGabriel/E4-1.AcevedoEnsisoPedroGabriel/ArbolBinarioOrdenado.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E4_1.AcevedoEnsisoPedroGabriel
+{
+    class ArbolBinarioOrdenado//encontre este ejemplo de un arbol binario esta clase es la que se usa para implementar un arbol binario
+    {
+        class Nodo//se implementa una clase nodo
+        {
+            public int info;
+            public Nodo izq, der;
+        }
+        Nodo raiz;//se instancia ese nodo con un nombre raiz
+
+        public ArbolBinarioOrdenado()//constructor de la clase arbol binario ordenado 
+        {
+            raiz = null;
+        }
+
+        public void Insertar(int info)//llamando este metodo insertas nuevos elementos al arbol en este caso se insertan elementos enteros
+        {
+            Nodo nuevo;
+            nuevo = new Nodo();
+            nuevo.info = info;
+            nuevo.izq = null;
+            nuevo.der = null;
+            if (raiz == null)//entiendo que si la raiz no tiene ningun elemento entonces se guarda en el
+                raiz = nuevo;
+            else//de no ser asi se guardara en el que siga
+            {
+                Nodo anterior = null, reco;
+                reco = raiz;
+                while (reco != null)
+                {
+                    anterior = reco;
+                    if (info < reco.info)
+                        reco = reco.izq;
+                    else
+                        reco = reco.der;
+                }
+                if (info < anterior.info)
+                    anterior.izq = nuevo;
+                else
+                    anterior.der = nuevo;
+            }
+        }
+
+
+        private void ImprimirPre(Nodo reco)//por lo que entiendo este es el metodo para imprimir en preorden al parecer este es un metodo recursivo
+        {
+            if (reco != null)
+            {
+                Console.Write(reco.info + " ");
+                ImprimirPre(reco.izq);//aqui se vuelve a llamar asi mismo este metodo
+                ImprimirPre(reco.der);
+            }
+        }
+
+        public void ImprimirPre()//este metodo manda a llamar el metodo recursivo ImprimirPre
+        {
+            ImprimirPre(raiz);//comenzando desde la raiz (el elemento de mas arriba)
+            Console.WriteLine();
+        }
+
+        private void ImprimirEntre(Nodo reco)//metodo recursivo con el que imprimimos los elementos en orden in-orden
+        {
+            if (reco != null)
+            {
+                ImprimirEntre(reco.izq);
+                Console.Write(reco.info + " ");
+                ImprimirEntre(reco.der);
+            }
+        }
+
+        public void ImprimirEntre()//este es el metodo inicial que mandara a llamar la recursividad
+        {
+            ImprimirEntre(raiz);//en este caso el primer elemento que se va a imprimir sera el elemento que este en la parte inferior izquierda
+            Console.WriteLine();
+        }
+
+
+        private void ImprimirPost(Nodo reco)//metodo recursivo que se usa para imprimir en pos-orden
+        {
+            if (reco != null)
+            {
+                ImprimirPost(reco.izq);
+                ImprimirPost(reco.der);
+                Console.Write(reco.info + " ");
+            }
+        }
+
+
+        public void ImprimirPost()//metodo que inicialmete mandara a llamar el metodo recursivo
+        {
+            ImprimirPost(raiz);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ E4-1.AcevedoEnsisoPedroGabriel/E4-1.AcevedoEnsisoPedroGabriel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E4_1.AcevedoEnsisoPedroGabriel
+{
+    class Program///este es un programa donde se da un ejemplo de el manejo de un arbol binario
+    {
+        static void Main(string[] args)
+        {
+            ArbolBinarioOrdenado abo = new ArbolBinarioOrdenado();//creamos un arbol binario llamado abo
+            abo.Insertar(100);//insertamos los elementos este primer elemento sera nuestra raiz
+            abo.Insertar(50);
+            abo.Insertar(25);
+            abo.Insertar(75);
+            abo.Insertar(150);
+            Console.WriteLine("Impresion preorden: ");
+            abo.ImprimirPre();
+            Console.WriteLine("Impresion entreorden: ");
+            abo.ImprimirEntre();
+            Console.WriteLine("Impresion postorden: ");
+            abo.ImprimirPost();
+            Console.ReadKey();
+            //este ejemplo que utilize simplemente se le agregan elementos a un arbol binario y los imprime en posorden, inorden y preorden
+            //basandome en como imprimimos los elementos durante este ejemplo dedusco que el arbol creado tiene esta forma
+            //          100
+            //        /     \
+            //      50       150
+            //    /    \
+            //  25      75
+        }
+    }
+}
